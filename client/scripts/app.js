20ead8dcd4868426e99f863e39ea0dd8312debb8
Mike Frazier <michael.s.frazier@gmail.com>
2016-01-25 19:27:43 -0600

    Added form to index.html. Continuing to implement app.handleSubmit method.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,7 +1,15 @@
 // YOUR CODE HERE:
 
 var app = {
-  init: function() {},
+  init: function() { 
+    console.log('in init');
+    $('#send .submit').on('submit', function(event) {
+      event.preventDefault(); // prevents page reload
+      console.log('trying to submit');
+      console.log(document.forms);
+      app.handleSubmit('Why so many Mel Brooks quotes?');
+    });
+  },
   send: function(message) {
     $.ajax({
       url: this.server,
@@ -33,12 +41,24 @@ var app = {
     $('#chats').children().remove();
   },
   addMessage: function(message) {
-    var userName = '<span class="userName">' + message.username + '</span>';
-    var text = '<span class="message">' + message.text + '</span>';
-    var roomName = '<span class="roomName">' + message.roomname + '</span>';
-    $('#chats').append('<p>' + userName + text + roomName + '</p>');
+    var userName = $('<span class="username">' + message.username + '</span>');
+    var text = $('<span class="message">' + message.text + '</span>');
+    var roomName = $('<span class="roomName">' + message.roomname + '</span>');
+    var div = $('<div></div>');
+    userName.on('click', function() {
+      app.addFriend();
+    });
+    div.append(userName, text, roomName);
+    $('#chats').append(div);
   },
   addRoom: function(room) {
     $('#roomSelect').append('<p>' + room + '</p>');
+  },
+  addFriend: function() {},
+  handleSubmit: function(message) {
+    var text = $('#message').val();
+    console.log(text);
+    app.send(text);
+    console.log("This is handleSubmit");
   }
 };
