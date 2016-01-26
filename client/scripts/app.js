b31d13b4e836e1850fc986e8378f03cc5d19b98c
Mike Frazier <michael.s.frazier@gmail.com>
2016-01-25 18:12:39 -0600

    Added div with id of chats to index.html. Implemented methods in app.js to send, fetch, clear chat field, add chats, and add rooms.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,44 @@
 // YOUR CODE HERE:
 
+var app = {
+  init: function() {},
+  send: function(message) {
+    $.ajax({
+      url: this.server,
+      type: "POST",
+      data: JSON.stringify(message),
+      contentType: "application/json",
+      success: function(data) {
+        console.log("chatterbox: Message Sent! data: ", data);
+      },
+      error: function(data) {
+        console.log("chatterbox: Failed to send message. Error: ", data);
+      }
+    });
+  },
+  fetch: function() {
+    $.ajax({
+      url: this.server,
+      type: "GET",
+      data: "",
+      dataType: "json",
+      success: function(data) {
+        console.log("chatterbox: Fetched! data: ", data);
+      }
+      // error: function() {}
+    });
+  },
+  server: "https://api.parse.com/1/classes/chatterbox",
+  clearMessages: function() {
+    $('#chats').children().remove();
+  },
+  addMessage: function(message) {
+    var userName = '<span class="userName">' + message.username + '</span>';
+    var text = '<span class="message">' + message.text + '</span>';
+    var roomName = '<span class="roomName">' + message.roomname + '</span>';
+    $('#chats').append('<p>' + userName + text + roomName + '</p>');
+  },
+  addRoom: function(room) {
+    $('#roomSelect').append('<p>' + room + '</p>');
+  }
+};
