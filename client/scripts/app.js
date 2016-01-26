115e0e80876fdd4211d0a096960cf598612f59cd
Mike Frazier <michael.s.frazier@gmail.com>
2016-01-26 16:37:33 -0600

    Continued implementation of app.handleSubmit. Included class in CSS to handle bold when friended.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,5 +1,5 @@
 // YOUR CODE HERE:
-
+var chatRoomNames = {};
 var app = {
   server: "https://api.parse.com/1/classes/chatterbox",
   init: function() {
@@ -7,7 +7,15 @@ var app = {
       event.preventDefault(); // prevents page reload
       app.handleSubmit();
     });
-
+    $('.clearChat').on('click', function(event) {
+      event.preventDefault();
+      app.clearMessages();
+    });
+    $('.refreshChat').on('click', function(event) {
+      event.preventDefault();
+      app.clearMessages();
+      app.fetch();
+    });
     this.fetch();
   },
   send: function(message) {
@@ -24,16 +32,23 @@ var app = {
       }
     });
   },
-  fetch: function() {
+  fetch: function(chatroom) {
+    chatroom = chatroom || "lobby";
     $.ajax({
       url: this.server,
       type: "GET",
-      data: "",
+      data: {order: "-createdAt"}, //, where: { roomname: "lobby" }
       dataType: "json",
       success: function(data) {
         console.log("chatterbox: Fetched! data: ", data);
+        $('#roomSelect').children().remove();
         for(var i = 0; i < 5; i++) {
           app.addMessage(data.results[i]);
+          if(!(data.results[i].roomname in chatRoomNames)) {
+            console.log(data.results[i].roomname);
+            app.addRoom(data.results[i].roomname);
+            chatRoomNames[data.results[i].roomname] = data.results[i].roomname;
+          }
         }
       }
       // error: function() {}
@@ -43,28 +58,42 @@ var app = {
     $('#chats').children().remove();
   },
   addMessage: function(message) {
-    console.log(message);
-    message.text = message.text || message.message;
+    message.text = (message.text || message.message || "").replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;');
+    message.roomname = (message.roomname || "All Rooms").trim();
     var userName = $('<span class="username">' + message.username + '</span>');
     var text = $('<span class="message">' + message.text + '</span>');
-    if(message.roomname) {
-      var roomName = $('<span class="roomName">' + message.roomname + '</span>');
-    }
+    var roomName = $('<span class="roomName">' + message.roomname + '</span>');
     var div = $('<div></div>');
+
+    div.addClass(message.roomname);
+    userName.addClass(message.username);
+
     userName.on('click', function() {
-      app.addFriend();
+      app.addFriend($(this).attr('class').split(" ")[1]);
     });
+
     div.append(userName, text, roomName);
     $('#chats').append(div);
   },
   addRoom: function(room) {
-    $('#roomSelect').append('<p>' + room + '</p>');
+    var newOption = $('<option>' + room + '</option>');
+    $('#roomSelect').append(newOption);
   },
-  addFriend: function() {},
+  addFriend: function(username) {
+    $('.' + username).addClass('friends');
+  },
   handleSubmit: function() {
-    console.log(message);
-    //var text = $('#message').val();
-    app.send(text);
+    var messageObj = {
+      text: $('#message').val(),
+      username: window.location.search.split('username=')[1],
+      roomname: $('#roomSelect').val()
+    };
+
+    app.send(messageObj);
+    app.clearMessages();
+    $('#roomSelect').children().remove();
+    chatRoomNames = {};
+    app.fetch();
   }
 };
 
